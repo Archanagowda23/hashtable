a8e5b6e5b780fff9bc72fa1674380cd21c89686a
Archanagowda23 <archanar249@gmail.com>
2022-03-27 20:46:08 +0530

    added key and vavlue pair

--- src/com/bridgelabz/hashtable/Main.java
+++ src/com/bridgelabz/hashtable/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to hashtable program");
+        Map<Integer, String> map = new Map<Integer, String>();
+        map.add(1, "aa");
+        map.add(2, "bb");
+        map.add(3, "vc");
+        map.add(4, "hh");
+        map.add(5, "mm");
+        map.add(6, "kk");
+        map.add(7, "pp");
+        map.add(8, "ii");
+        map.add(9, "uu");
+        map.size();
     }
 }
--- src/com/bridgelabz/hashtable/Map.java
+++ src/com/bridgelabz/hashtable/Map.java
@@ -1,5 +1,60 @@
 package com.bridgelabz.hashtable;
 
+import java.util.ArrayList;
+
 public class Map<K,V> {
+    public ArrayList<HashNode<K, V>> chainArray;
+    // capacity of array list
+    public int arrCapacity;
+    // size of array list
+    public int size;
+
+    // constructor( initializing capacity, size and
+    // empty chain
+    public Map() {
+        chainArray = new ArrayList<>();
+        arrCapacity = 11;
+        size = 0;
+
+        for (int i = 0; i < arrCapacity; i++) {
+            chainArray.add(null);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int hashIndex(K key) {
+        int hashCod = (int) key;
+        return hashCod;
+    }
+
+    // Returns value corresponding to the key
+
+    public void add(K key, V value) {
+        int hashIndex = hashIndex(key);
+        // Get head of chain
+        HashNode<K, V> head = chainArray.get(hashIndex);
+        // Check if key is already present
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        // Insert key in chain
+        size++;
+        head = chainArray.get(hashIndex);
+        HashNode<K, V> newNode = new HashNode<K, V>(key, value);
+        newNode.next = head;
+        chainArray.set(hashIndex, newNode);
+
+    }
 
 }
