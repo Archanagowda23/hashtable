9d96a0e863a3faa94d5f71845c5880d6636cc192
Archanagowda23 <archanar249@gmail.com>
2022-03-27 20:41:58 +0530

    initial commit

---
+++ src/com/bridgelabz/hashtable/HashNode.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.hashtable;
+
+public class HashNode<K,V> {
+    K key;
+    V value;
+    HashNode<K, V> next;
+
+    HashNode(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
